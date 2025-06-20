9f83fa5861faccc1ffa3813cedfe5bc58b01c5f7
SebaVCH <sebastianvchepillo@gmail.com>
2025-06-20 13:16:21 -0400

    Feat: Docker environment implemented
    - Setup Dockerfiles for backend and frontend services and a docker-compose.
    
    - Updated CORS middleware to allow frontend access based on environment variable.
    
    - Removed usage of godotenv.
    
    - Added docker in CICD.yml

--- Backend/internal/config/env.go
+++ Backend/internal/config/env.go
@@ -1,17 +1,12 @@
 package config
 
 import (
-	"github.com/joho/godotenv"
 	"os"
 )
 
 var JwtSecret []byte
 
 func LoadEnv() error {
-
-	if err := godotenv.Load(".env"); err != nil {
-		return err
-	}
 
 	JwtSecret = []byte(os.Getenv("JWT_SECRET"))
 
--- Backend/internal/interfaces/middleware/corsMiddleware.go
+++ Backend/internal/interfaces/middleware/corsMiddleware.go
@@ -3,15 +3,15 @@ package middleware
 import (
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"os"
 )
 
 func CORSMiddleware() gin.HandlerFunc {
 	return cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:5173"},
+		AllowOrigins:     []string{os.Getenv("FRONTEND_URL")},
 		AllowMethods:     []string{"OPTIONS", "PUT", "GET", "POST", "DELETE"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
 		AllowCredentials: true,
 	})
 }
-
